5a257029f050e77cb8c964a3fd16dfeaf05f6b2d
sajadevo <nawabisajadahmad@gmail.com>
2023-10-18 11:35:38 +0300

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import "./globals.css";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
@@ -10,9 +11,9 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "Material Tailwind | Portfolio Template",
+  title: "Next.js + Tailwind Portfolio Page",
   description:
-    "Portfolio template built with Material Tailwind React and Next.js 13.",
+    "Introducing Tailwind Portfolio Page, a versatile and stylish portfolio template built on the foundation of Tailwind CSS and Material Tailwind.",
 };
 
 export default function RootLayout({
@@ -22,6 +23,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          defer
+          data-site="YOUR_DOMAIN_HERE"
+          src="https://api.nepcha.com/js/nepcha-analytics.js"
+        ></script>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      </head>
       <body className={roboto.className}>
         <Layout>{children}</Layout>
       </body>
