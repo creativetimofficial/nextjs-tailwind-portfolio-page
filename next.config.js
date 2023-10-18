18b44f05e04df62c3c3f44a3bbfe735b4e5fbfc4
sajadevo <nawabisajadahmad@gmail.com>
2023-10-18 11:57:02 +0300

    ready to deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/nextjs-tailwind-portfolio-page",
 };
 
 module.exports = nextConfig;
