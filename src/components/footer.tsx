861a5eabef8df3981f21f819a8ad0fa7d683f90a
sajadevo <nawabisajadahmad@gmail.com>
2023-11-20 09:54:11 +0430

    update product

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,15 @@ export function Footer() {
       <div className="container mx-auto">
         <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 border-t border-gray-200 py-6 md:justify-between">
           <Typography className="text-center font-normal !text-gray-700">
-            &copy; {CURRENT_YEAR} Creative Tim™. All Rights Reserved.
+            &copy; {CURRENT_YEAR} Made with{" "}
+            <a href="https://www.material-tailwind.com" target="_blank">
+              Material Tailwind
+            </a>{" "}
+            by{" "}
+            <a href="https://www.creative-tim.com" target="_blank">
+              Creative Tim
+            </a>
+            .
           </Typography>
           <ul className="flex gap-8 items-center">
             {LINKS.map((link) => (
